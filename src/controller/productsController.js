4519ed0f6b717d5ae149324de5570f8df54c0d66
Luciano <luciano.carrizo26@gmail.com>
2023-08-24 18:35:40 -0300

    lista y detalles

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -27,20 +27,3 @@ const controller = {
 };
 
 module.exports = controller;
-
-
-
-/* module.exports = {
-    cart : (req, res) => {
-       return res.render('cart');
-      },
-      details : (req, res) => {
-        return res.render('details');
-       },
-       add : (req, res) => {
-        return res.render('productAdd');
-       },
-       edit : (req, res) => {
-        return res.render('productEdit');
-       },
-} */
