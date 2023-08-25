57d4459d9cb49e52aaf26d58e4aaa7bd006880b4
Luciano <luciano.carrizo26@gmail.com>
2023-08-25 14:22:57 -0300

    Fix: Json y carrousel home

--- src/controller/indexControllers.js
+++ src/controller/indexControllers.js
@@ -7,8 +7,24 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
 	index: (req, res) => {
+		const product = products.find(product => product.id === +req.params.id)
+		const productHombre = products.filter(product => product.category == "hombre")
+		const productMujer = products.filter(product => product.category == "mujer")
+		const productNene = products.filter(product => product.category == "nene")
 		return res.render('index', {
-			products
+			products,
+			product,
+			productHombre,
+			productMujer,
+			productNene
+		})
+	},
+
+	search: (req, res) => {
+		const results = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()))
+		return res.render('index', {
+			products,
+			keywords : req.query.keywords
 		})
 	}
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 /* / */
 router.get('/', indexControllers.index);
+router.get('/search', indexControllers.search); 
 
 module.exports = router;
