0647ee580c1dbbf1cb2ef957ae892ecd205af3b7
Luciano <luciano.carrizo26@gmail.com>
2023-09-10 20:29:49 -0300

    accesibilidad rutas de productos

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const productsController = require('../controller/productsController');
 const { upload } = require('../middlewares/upload');
+const adminCheck = require('../middlewares/adminCheck');
 const router = express.Router();
 
 /* /produtcs */
@@ -8,14 +9,14 @@ router.get('/cart', productsController.cart)
 router.get('/details/:id', productsController.details)
 
 /* CREATE PRODUCT */
-router.get('/add', productsController.add)
+router.get('/add', adminCheck, productsController.add)
 router.post('/add', upload.fields([
   { name: "image", maxCount: 1 }, // image primary
   { name: "images"}, // images secondary
 ]), productsController.create)
 
 /* EDIT PRODUCT */ 
-router.get('/edit/:id', productsController.edit); 
+router.get('/edit/:id', adminCheck, productsController.edit); 
 router.put(
   "/edit/:id",
   upload.fields([
