0828c7503c13281115cf9eccb1763c830f6cef0d
Lucas Arroyo <lucas.aa68@gmail.com>
2023-10-24 18:03:33 -0300

    add: validator edit

--- src/validations/productEditValidator.js
+++ src/validations/productEditValidator.js
@@ -1,16 +1,38 @@
-const {check} = require('express-validator')
+const { check, body } = require("express-validator");
+const db = require("../database/models");
 
 module.exports = [
-    check('name')
-    .notEmpty().withMessage('Nombre del producto es obligatorio')
+  check("name")
+    .notEmpty()
+    .withMessage("Nombre del producto es obligatorio")
     .isLength({
-        min:2,
-    }).withMessage('Tiene que se al menos 2 caracteres'),
-    check('color')
-    .notEmpty().withMessage('El color tiene que se obligatorio'),
-    check('price')
-    .notEmpty().withMessage('Precio es obligatorio')
+      min: 2,
+    })
+    .withMessage("Tiene que se al menos 2 caracteres"),
+  check("hexColor").notEmpty().withMessage("El hex. color es obligatorio"),
+  check("textColor")
+    .notEmpty()
+    .withMessage("El texto del color es obligatorio"),
+  check("price")
+    .notEmpty()
+    .withMessage("Precio es obligatorio")
     .isInt({
-        get: 1,
-    }).withMessage('Precio tiene que se un numero')
-]
+      get: 1,
+    })
+    .withMessage("Precio tiene que se un numero"),
+  body("images")
+    .custom(async (value, { req }) => {
+      const product = await db.Product.findByPk(req.params.id, {
+        include: ["images"],
+      });
+      const { rememberImg } = req.body;
+      if (
+        (rememberImg &&
+          product.images.length + req.files?.images?.length > 6) ||
+        (!rememberImg && newImages.length > 6)
+      ) {
+        throw new Error("La cantidad maxima de imágenes en el producto son 6.");
+      }
+      return false;
+    })
+];
