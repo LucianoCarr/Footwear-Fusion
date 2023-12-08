2af2e348ede8349f043eef8b963c8d4aa0d69d84
ariel.boxler1997@gmail.com <ariel.boxler@gmail.com>
2023-12-08 18:12:04 -0300

    destroyImg

--- src/controller/products/destroy.js
+++ src/controller/products/destroy.js
@@ -4,11 +4,13 @@ const path = require('path')
 //const destroyProduct = require('../../services/productsServices/destroy.Services');
 
 module.exports = async (req, res) => {
-  try {
+  /*try {
     const productId = req.params.id;
     const product = await db.Product.findByPk(productId)
     const images = await db.Image.findAll({where:{productId}})
 
+    
+    
     const pathFile = path.join(__dirname,`../../../public/img/${product.image}`)
     const existFile = fs.existsSync(pathFile)
     if(existFile) {
@@ -29,5 +31,38 @@ module.exports = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.log(error);
+  }*/
+
+  try {
+    const productId = req.params.id;
+    const product = await db.Product.findByPk(productId);
+    const images = await db.Image.findAll({ where: { productId } });
+
+    // Eliminar las imágenes cargadas por el usuario
+    for (const img of images) {
+      if (img.filename !== 'default-image.png') {
+        const pathFile = path.join(__dirname, `../../../public/img/${img.filename}`);
+        const existFile = fs.existsSync(pathFile);
+        if (existFile) {
+          fs.unlinkSync(pathFile);
+        }
+        await img.destroy();
+      }
+    }
+
+    // Eliminar el producto
+    const pathFile = path.join(__dirname, `../../../public/img/${product.image}`);
+    const existFile = fs.existsSync(pathFile);
+    if (existFile && product.image !== 'default-image.png') {
+      fs.unlinkSync(pathFile);
+    }
+
+    await product.destroy();
+
+    return res.redirect('/');
+  } catch (error) {
+    console.log(error);
+    
+    res.status(500).send('Error del servidor');
   }
 };
