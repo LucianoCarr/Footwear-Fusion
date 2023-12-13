bd4fbbeecde8f76f104a2a14e1b6aae864cc0e85
Luciano <luciano.carrizo26@gmail.com>
2023-12-13 15:08:59 -0300

    fix: mas detalles

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,6 @@
 
 window.onload = function () {
-    /* Search */
+    // Search
    document.getElementById('form-search').addEventListener('submit',function(e){
         e.preventDefault()
 
@@ -9,7 +9,7 @@ window.onload = function () {
 
         for (let i = 0; i < elementForm.length; i++) {
 
-            if (!elementForm[i].value.trim() || elementForm[i].classList.contains('is-invalid')){
+            if (!elementForm[i].value.trim()  || elementForm[i].classList.contains('is-invalid')){
                 elementForm[i].classList.add('is-invalid')
                document.getElementById('errores').innerHTML = 'No puede enviarse vacio'
                 error = true
